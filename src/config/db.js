6f8aa73e34e37f81b8fdbb439d95cebbbe05db10
man-in-dev <man.in.dev.16@gmail.com>
2025-08-10 22:48:42 +0530

    add env

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
     try {
-        const conn = await mongoose.connect("mongodb+srv://manindev16:LMTJB3qNF2QKPG6x@cluster0.spmnm7o.mongodb.net/todo");
+        const conn = await mongoose.connect(process.env.DATABASE_URI);
         console.log(`Database connected successfully`)
     } catch (error) {
         console.log(`Database connection failed: ${error.message}`)
